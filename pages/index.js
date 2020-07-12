f225509b7b81733b318a00b5a0d1021f07e02698
Jonathan Parton <jon.parton@gmail.com>
2020-07-12 13:25:12 +0100

    Added some more detailed examples

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import SmallCard from '../components/SmallCard'
-import { projectIcons } from '../components/Icons'
-
+import SmallCard from "../components/SmallCard";
+import { projectIcons } from "../components/Icons";
 
 const Home = () => (
   <div className="home">
@@ -8,9 +7,18 @@ const Home = () => (
     <div className="card-grid">
       <a className="card-small" href={`/personManuals`}>
         <h3>People Instruction Manuals</h3>
-    </a>
+        <p>An example of client side rendered content served up by azure functions. This is how to interact with server side code from a "static" page.</p>
+      </a>
+      <a className="card-small" href={`/project/projects`}>
+        <h3>Projects</h3>
+        <p>An example of pre-rendered pages which could be usefull for things like blogs or slowly changing data such as tornament results.</p>
+      </a>
+      <a className="card-small" href={`/about`}>
+        <h3>About</h3>
+        <p>An example of a completely static page with that is prerendered and server super fast bu the Azure Static Web App</p>
+      </a>
     </div>
   </div>
-  )
+);
 
-export default Home
+export default Home;
