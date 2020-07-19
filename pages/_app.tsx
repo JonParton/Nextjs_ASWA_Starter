bc5e3f83591ca922c5035d6336f07eafd575863d
Jonathan Parton <jon.parton@gmail.com>
2020-07-19 13:01:08 +0100

    Fixed an issue with Hot Reload and Next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,13 @@ import { CssBaseline } from '@material-ui/core';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <div>
       <CssBaseline />
