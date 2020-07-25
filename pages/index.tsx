3f50841a772e183b128c3ac78ab69b89545fadea
Jonathan Parton <jon.parton@gmail.com>
2020-07-26 00:18:59 +0100

    Switched order of Index Cards to make CSR more !!

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,35 +55,35 @@ function Home() {
         </Grid>
         {/* Card 1 */}
         <IndexCard
-          title="Fully Static Page (SSG)"
-          subTitle="Just like plain HTML but with the power of React!"
-          description={`You can create a page using all the power of react such as the
-                extensive libraries available, map functions and extracting
-                HTML out into custom components etc. However Next.js will pre
-                render the page for you at build time
-                and serve it up super quick from the server!`}
-          exampleLink="/staticPageExample"
-          imageURL="/images/undraw_static_assets_rpm6.png"
-        />
-        {/* Card 2 */}
-        <IndexCard
-          title="Dynamic Route, Sever Rendered (SSR)"
-          subTitle="One page template, Many Server Rendered Pages"
-          description={`If you would like to create many pages from a set of data that you know 
-                at build time you can use Next.js's dynamic routing along with Static Props to pre-render 
-                each page at build time. This could be useful for things like Blog posts or tournament results 
-                where you want each page to have its own URL and data doesn't change often. `}
-          exampleLink="/DeployableFramework/PreRenderedDynamicRoutesExample"
-          imageURL="/images/undraw_next_js_8g5m.png"
-        />
-        {/* Card 3 */}
-        <IndexCard
           title="SSR Frame, CSR Content!"
           subTitle="Let Next.js render the frame on the server then load content dynamically on the client side."
           description={`An example of client side rendered content served up by azure functions. This is how to interact with server side code from a "static" page.`}
           exampleLink="/personManuals"
           imageURL="/images/undraw_react_y7wq.png"
         />
+        {/* Card 2 */}
+        <IndexCard
+          title="Dynamic Route, Sever Rendered (SSR)"
+          subTitle="One page template, Many Server Rendered Pages"
+          description={`If you would like to create many pages from a set of data that you know 
+          at build time you can use Next.js's dynamic routing along with Static Props to pre-render 
+          each page at build time. This could be useful for things like Blog posts or tournament results 
+          where you want each page to have its own URL and data doesn't change often. `}
+          exampleLink="/DeployableFramework/PreRenderedDynamicRoutesExample"
+          imageURL="/images/undraw_next_js_8g5m.png"
+        />
+        {/* Card 3 */}
+          <IndexCard
+            title="Fully Static Page (SSG)"
+            subTitle="Just like plain HTML but with the power of React!"
+            description={`You can create a page using all the power of react such as the
+                  extensive libraries available, map functions and extracting
+                  HTML out into custom components etc. However Next.js will pre
+                  render the page for you at build time
+                  and serve it up super quick from the server!`}
+            exampleLink="/staticPageExample"
+            imageURL="/images/undraw_static_assets_rpm6.png"
+          />
       </Grid>
       <Grid xs={false} sm={2} item className="Gutter"></Grid>
     </Grid>
