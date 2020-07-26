59cca839e106e68a74dd42b3e7a552282604b602
Jonathan Parton <jon.parton@gmail.com>
2020-07-26 22:00:37 +0100

    First fix for strict mode - Backed out for now!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,7 +42,7 @@ export const MyApp: React.FunctionComponent<AppProps> = ({
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles) {
+    if (jssStyles && jssStyles.parentElement) {
       jssStyles.parentElement.removeChild(jssStyles)
     }
   }, [])
