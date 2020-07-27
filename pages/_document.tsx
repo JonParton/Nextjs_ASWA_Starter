fde138a9aab5b8369855bf8c347995959399adf9
Jonathan Parton <jon.parton@gmail.com>
2020-07-27 11:47:14 +0100

    Fixed ESLint Warning in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core'
 class MyDocument extends Document {
-  render() {
+  render(): JSX.Element {
     return (
       <Html lang="en">
         <Head>
